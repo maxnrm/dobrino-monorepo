7d085147151a533df7d53be11916e8d3d6583305
Maxim Nurmukhametov <maxim.nurmukhametov@gmail.com>
2024-08-05 08:55:29 +0500

    add broadcast method

---
+++ internal/models/broadcast.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"dobrino/internal/pg"
+	"errors"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+type Broadcast struct {
+	// broadcastMessages []BroadcastMessage
+}
+
+func (b *Broadcast) Broadcast(bot *tele.Bot, db *pg.PG) error {
+	dbMsg, err := db.GetBroadcastMessageForSend()
+	if err != nil {
+		return errors.New("no message for broadcast provided")
+	}
+
+	msg, err := floodMessageFromDBBroadcastMessage(dbMsg)
+	if err != nil {
+		return err
+	}
+
+	dbUsers, err := db.GetUsers()
+	if err != nil {
+		return err
+	}
+
+	if len(dbUsers) == 0 {
+		return errors.New("no users to broadcast message to")
+	}
+
+	for _, dbUser := range dbUsers {
+
+		u, err := User{}.FromDB(dbUser)
+		if err != nil {
+			continue
+		}
+
+		bot.Send(u, msg)
+	}
+
+	return nil
+}
+
+// type BroadcastMessage struct {
+// 	message *fm.FloodMessage
+// }
