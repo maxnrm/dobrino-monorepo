c9bdec889c81a032418caacc6fd34855f1e23b0d
Maxim Nurmukhametov <maxim.nurmukhametov@gmail.com>
2024-08-05 08:39:18 +0500

    return error on goldmark failed parse

--- internal/models/helpers.go
+++ internal/models/helpers.go
@@ -12,6 +12,60 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
+func floodMessageFromDBBroadcastMessage(dbMessage *dbmodels.BroadcastMessage) (*fm.FloodMessage, error) {
+
+	// set parseMode HTML as default
+	// as we are using goldmark and telegold
+	message := &fm.FloodMessage{
+		SendOptions: &tele.SendOptions{
+			ParseMode: tele.ModeHTML,
+		},
+	}
+
+	var msgBuf bytes.Buffer
+	md := goldmark.New(goldmark.WithRenderer(telegold.NewRenderer()))
+
+	switch {
+	case dbMessage.Image != nil && dbMessage.Message != nil:
+		photoURL := config.IMGPROXY_PUBLIC_URL + *dbMessage.Image
+		err := md.Convert([]byte(*dbMessage.Message), &msgBuf)
+		if err != nil {
+			return nil, err
+		}
+
+		message.Photo = &tele.Photo{
+			File:    tele.File{FileURL: photoURL},
+			Caption: msgBuf.String(),
+		}
+
+		message.Type = fm.Photo
+
+		return message, nil
+	case dbMessage.Image != nil:
+		photoURL := config.IMGPROXY_PUBLIC_URL + *dbMessage.Image
+
+		message.Photo = &tele.Photo{
+			File: tele.File{FileURL: photoURL},
+		}
+
+		message.Type = fm.Photo
+
+		return message, nil
+	case dbMessage.Message != nil:
+		err := md.Convert([]byte(*dbMessage.Message), &msgBuf)
+		if err != nil {
+			return nil, err
+		}
+
+		text := msgBuf.String()
+		message.Text = &text
+		message.Type = fm.Text
+		return message, nil
+	}
+
+	return nil, errors.New("invalid button data")
+}
+
 func floodMessageFromDBButton(dbButton *dbmodels.Button) (*fm.FloodMessage, error) {
 	// set parseMode HTML as default
 	// as we are using goldmark and telegold
@@ -27,7 +81,10 @@ func floodMessageFromDBButton(dbButton *dbmodels.Button) (*fm.FloodMessage, erro
 	switch {
 	case dbButton.Image != nil && dbButton.Message != nil:
 		photoURL := config.IMGPROXY_PUBLIC_URL + *dbButton.Image
-		md.Convert([]byte(*dbButton.Message), &msgBuf) // just use it as usual
+		err := md.Convert([]byte(*dbButton.Message), &msgBuf) // just use it as usual
+		if err != nil {
+			return nil, err
+		}
 
 		message.Photo = &tele.Photo{
 			File:    tele.File{FileURL: photoURL},
@@ -48,7 +105,11 @@ func floodMessageFromDBButton(dbButton *dbmodels.Button) (*fm.FloodMessage, erro
 
 		return message, nil
 	case dbButton.Message != nil:
-		md.Convert([]byte(*dbButton.Message), &msgBuf) // just use it as usual
+		err := md.Convert([]byte(*dbButton.Message), &msgBuf) // just use it as usual
+		if err != nil {
+			return nil, err
+		}
+
 		text := msgBuf.String()
 		message.Text = &text
 		message.Type = fm.Text
